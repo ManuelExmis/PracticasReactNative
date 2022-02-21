6fb36ad56afe67d7497b08037a59e5471b005d16
ManuelExmis <lmanuelex@gmail.com>
2022-02-21 06:01:43 -0600

    primer commit

---
+++ Sección 12 - NodeJS/serverless/api/auth/index.js
@@ -0,0 +1,33 @@
+const jwt = import('jsonwebtoken')
+const Users = require('../models/Users')
+
+const isAuthenticated = (req, res, next) => {
+	const token = req.headers.authorization
+
+	if (!token) {
+		return res.sendStatus(403)
+	}
+
+	jwt.verify(token, 'mi-secreto', (err, decoded) => {
+		const { _id } = decoded
+
+		Users.findOne({ _id }).exec()
+			.then(user => {
+				req.user = user
+				return next()
+			})
+	})
+}
+
+const hasRoles = roles => (req, res, next) => {
+  if (roles.indexOf(req.user.role > -1)) {
+    return next()
+  }
+
+  res.sendStatus(403);
+}
+
+module.exports = {
+  isAuthenticated,
+  hasRoles
+}
